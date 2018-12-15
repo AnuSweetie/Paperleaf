ad25f750daba7984376fe4a2c35f8a317b7c8525
AnuSweetie <nmkraj50@gmail.com>
2018-12-15 16:22:02 +0530

    Add files via upload

---
+++ PaperLeaf 3.0/js/jscript.js
@@ -0,0 +1,40 @@
+// Jarvis Javascript
+
+
+// Google Search JavaScript
+// Created by Google Inc.
+// Used By Infinity Inc.
+var domainroot="www.google.com"
+	function Gsitesearch(curobj){
+		curobj.q.value=" "+" "+" "+curobj.qfront.value
+}
+
+function myFunction() {
+    var d = new Date();
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    var n = weekday[d.getDay()];
+    document.getElementById("day").innerHTML = n;
+
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(myFunction, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
